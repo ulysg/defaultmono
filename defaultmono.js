421adb305b3091a7edf0b5435f028e4f05b17cd8
ulysg <compte@ulys.ch>
2024-04-16 14:11:48 +0200

    Reverted to old version

--- defaultmono.js
+++ defaultmono.js
@@ -1,31 +1,5 @@
-let root_style = getComputedStyle(document.querySelector(":root"));
-let selectors = [];
-
-let ff_pattern = /font-family:/;
-let mono_pattern = /mono/i;
-let var_pattern = /var\((.+)\)/; 
-
-for (const style of document.styleSheets) {
-  for (const rule of style.cssRules) {
-    if (ff_pattern.test(rule.cssText)) {
-      if (mono_pattern.test(rule.cssText)) {
-        selectors.push(rule.selectorText);
-        continue;
-      }
-      
-      let match = rule.cssText.match(var_pattern);
-
-      if (match.length > 1) {
-        let var_value = root_style.getPropertyValue(match[1]);
-
-        if (mono_pattern.test(var_value)) {
-          selectors.push(rule.selectorText);
-        }
-      }
+document.querySelectorAll("*").forEach(function(node) {
+    if (getComputedStyle(node).fontFamily.toLowerCase().includes("mono")) {
+        node.style.setProperty("font-family", "monospace", "important");
     }
-  }
-}
-
-for (let node of document.querySelectorAll(selectors.join(","))) {
-  node.style.setProperty("font-family", "monospace", "important");
-}
+});
