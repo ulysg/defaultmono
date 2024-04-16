82e86c557152e2cf8f72837fbf941e8240ef08f6
ulysg <compte@ulys.ch>
2024-04-16 14:00:50 +0200

    Added support for css var

--- defaultmono.js
+++ defaultmono.js
@@ -1,10 +1,27 @@
+let root_style = getComputedStyle(document.querySelector(":root"));
 let selectors = [];
-let pattern = /font-family:.*mono/i;
+
+let ff_pattern = /font-family:/;
+let mono_pattern = /mono/i;
+let var_pattern = /var\((.+)\)/; 
 
 for (const style of document.styleSheets) {
   for (const rule of style.cssRules) {
-    if (pattern.test(rule.cssText)) {
-      selectors.push(rule.selectorText);
+    if (ff_pattern.test(rule.cssText)) {
+      if (mono_pattern.test(rule.cssText)) {
+        selectors.push(rule.selectorText);
+        continue;
+      }
+      
+      let match = rule.cssText.match(var_pattern);
+
+      if (match.length > 1) {
+        let var_value = root_style.getPropertyValue(match[1]);
+
+        if (mono_pattern.test(var_value)) {
+          selectors.push(rule.selectorText);
+        }
+      }
     }
   }
 }
